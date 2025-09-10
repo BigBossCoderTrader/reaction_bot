9891c623aa7f07f98fd4896014cff8fdd0e51740
BIGBOSS <povchomreoun.yuleang.2824@rupp.edu.kh>
2025-09-10 08:57:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from telegram import Update, ReactionTypeEmoji
 from telegram.ext import Application, MessageHandler, filters, ContextTypes
 
@@ -5,7 +6,8 @@ BOT_TOKEN = "8435629423:AAFwtgLuifZdvcLchm18fA9lqtVpq6_Y9NU"
 
 async def on_channel_post(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.channel_post
-    emoji = "🔥"
+    emoji_list = ["🔥", "❤️", "👍", "👏", "😂"]  # add as many as you want
+    emoji = random.choice(emoji_list)  # pick one at random
     await context.bot.set_message_reaction(
         chat_id=msg.chat_id,
         message_id=msg.message_id,
